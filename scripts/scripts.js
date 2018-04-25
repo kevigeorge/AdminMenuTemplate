50c87542ca9f162dc2c8c7c03b707f61c9ee0879
kevigeorge <mail.kevi@gmail.com>
2018-04-25 18:09:14 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
 
+
+$(".main-menu-item").on("click",function(){
+   /* $(".sub-menu").css("display","block")*/
+     $('li > ul').not($(this).children("ul").toggle()).hide();
+});
+
+
+$(".drop-main-menu-item").on("click",function(){
+   /* $(".drop-sub-menu").css("display","block")*/
+     $('li > ul').not($(this).children("ul").toggle()).hide();
+});
